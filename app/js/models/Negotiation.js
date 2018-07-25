a6d7cc7e88d3ed25133d691d4a50f68da923047c
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-07-25 12:49:41 -0300

    Modeling the Negotiation class with ES6

---
+++ app/js/models/Negotiation.js
@@ -0,0 +1,20 @@
+class Negoatiation {
+
+  constructor(date, quantity, value) {
+    this._date = date;
+    this._quantity = quantity;
+    this._value = value;
+  }
+
+  get date() {
+    return this._date;
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  get value() {
+    return this._value;
+  }
+}
