ca60664ee4aa612d123635ad986812e6851c8463
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-06 13:18:54 -0300

    Retrieving data from the form

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,2 +1,5 @@
-const negotiation = new Negotiation(new Date(), 1, 100);
-console.log(negotiation);
+const controller = new NegotiationController();
+
+document
+  .querySelector('.form')
+  .addEventListener('submit', controller.add.bind(controller));
