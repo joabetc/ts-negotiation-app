89ef8a710af30226abed36e593b469ea095a30a3
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-08 13:21:48 -0300

    Using the constructor as a shortcut for class attributes

--- app/ts/models/Negotiation.ts
+++ app/ts/models/Negotiation.ts
@@ -1,14 +1,9 @@
 class Negotiation {
 
-  private _date: Date;
-  private _quantity: number;
-  private _value: number;
-
-  constructor(date: Date, quantity: number, value: number) {
-    this._date = date;
-    this._quantity = quantity;
-    this._value = value;
-  }
+  constructor(
+    private _date: Date, 
+    private _quantity: number, 
+    private _value: number) { }
 
   get date() {
     return this._date;
