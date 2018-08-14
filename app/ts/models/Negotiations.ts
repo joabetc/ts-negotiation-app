b3ca01ff4362ae87bc41544403943cd7735f04cb
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-14 11:35:21 -0300

    Creating new class for negotiations array

---
+++ app/ts/models/Negotiations.ts
@@ -0,0 +1,12 @@
+class Negotiations {
+
+  private _negotiations: Array<Negotiation> = [];
+
+  add(negotiation: Negotiation) {
+    this._negotiations.push(negotiation);
+  }
+
+  toArray() {
+    return this._negotiations;
+  }
+}
