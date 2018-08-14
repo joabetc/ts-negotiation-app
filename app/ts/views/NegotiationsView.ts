7c9a60928c7e45ca86681cb82c0bdadb297fdddf
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-14 13:04:03 -0300

    Creating a view class for rendering templates

---
+++ app/ts/views/NegotiationsView.ts
@@ -0,0 +1,20 @@
+class NegotiationsView {
+
+  template(): string {
+    return `
+    <table class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th>DATE</th>
+          <th>QUANTITY</th>
+          <th>VALUE</th>
+          <th>AMOUNT</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+      <tfoot>
+      </tfoot>
+    </table>`;
+  }
+}
