0e07d0e1a3e4099fafe93a62b00f2e4f3b6ce05c
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-14 12:35:44 -0300

    Setting returning type on the add method

--- app/ts/models/Negotiations.ts
+++ app/ts/models/Negotiations.ts
@@ -2,7 +2,7 @@ class Negotiations {
 
   private _negotiations: Negotiation[] = [];
 
-  add(negotiation: Negotiation) {
+  add(negotiation: Negotiation): void {
     this._negotiations.push(negotiation);
   }
 
