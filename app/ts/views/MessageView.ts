3e0a0c0276d46d5c291c3deca2e21b87aca0c6a1
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-17 13:18:30 -0300

    Adding export

--- app/ts/views/MessageView.ts
+++ app/ts/views/MessageView.ts
@@ -1,6 +1,6 @@
 import { View } from './View';
 
-class MessageView extends View<String> {
+export class MessageView extends View<String> {
 
   template(model: string): string {
     return `<p class="alert alert-info">${model}</p>`;
