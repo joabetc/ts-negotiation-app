2e646017f9b00d9d329673a55e71c5186df6a4f9
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-20 12:53:45 -0300

    Using optional parameters

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,17 @@
 export abstract class View<T> {
   
   private _element: JQuery;
+  private _escape: boolean;
 
-  constructor(selector: string) {
+  constructor(selector: string, escape?: boolean) {
     this._element = $(selector);
   }
 
   update(model: T): void {
-    this._element.html(this.template(model));
+    let template = this.template(model);
+    if (this._escape)
+      template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
+    this._element.html(template);
   }
 
   abstract template(model: T): string;
