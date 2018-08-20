ee2c000b66fc0a0a91637ea539257b98cd23d220
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-20 12:39:47 -0300

    Changing private properties to readonly

--- app/ts/models/Negotiation.ts
+++ app/ts/models/Negotiation.ts
@@ -1,23 +1,11 @@
 export class Negotiation {
 
   constructor(
-    private _date: Date, 
-    private _quantity: number, 
-    private _value: number) { }
-
-  get date() {
-    return this._date;
-  }
-
-  get quantity() {
-    return this._quantity;
-  }
-
-  get value() {
-    return this._value;
-  }
+    readonly date: Date, 
+    readonly quantity: number, 
+    readonly value: number) { }
 
   get amount() {
-    return this._quantity * this._value;
+    return this.quantity * this.value;
   }
 }
