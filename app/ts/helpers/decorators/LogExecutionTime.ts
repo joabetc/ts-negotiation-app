57645cf31f412a412d2f2c07d3b16fa4e8686ecc
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-08-23 12:49:31 -0300

    Using decorator for performance check

--- app/ts/helpers/decorators/LogExecutionTime.ts
+++ app/ts/helpers/decorators/LogExecutionTime.ts
@@ -5,8 +5,13 @@ export function LogExecutionTime() {
     const originalMethod = descriptor.value;
 
     descriptor.value = function(...args: any[]) {
-      
+      console.log('----------------');
+      console.log(`Parameters for ${propertyKey} method: ${JSON.stringify(args)}`);
+      const t1 = performance.now();
       const returnValue = originalMethod.apply(this, args);
+      const t2 = performance.now();
+      console.log(`Returned value from ${propertyKey} method: ${JSON.stringify(returnValue)}`);
+      console.log(`The ${propertyKey} method tooked ${t2 - t1}ms`);
       return returnValue;
     }
 
